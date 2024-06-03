959d5f23220cbb4567050ee70ae16eb2ea80e4c0
Martin Szabo <martin.szabo333@gmail.com>
2024-06-03 23:47:11 +0200

    5-1 Shenanigan

--- 5-1.c
+++ 5-1.c
@@ -1,62 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdbool.h>
+#include <limits.h>
+#include <assert.h>
+
+#define RANGE(value, upper, lower) ( value >= lower && value <= upper )
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+/**
+ * Zdroje:
+ * Cviko 2021 (https://drive.google.com/file/d/1eA7ZalviE8l9F8zhqJbM7zbTOt_JPZvQ/view)
+ * Cviko 2020 (https://drive.google.com/file/d/18GDCnZnV9qbI7A0Ko8es5po3D8i_tjYZ/view)
+ */
+
+// Kvoli tomu ze musime vyriesit jednu a jednu hned preskocit. Takze ked mame menej ako 2 tak mozeme riesit len prvu
+#define MIN_TASKS 2
+#define MAX_TASKS 50
+#define MIN_HAPP 0
+#define MAX_HAPP 1000
 
 typedef struct {
-	int n, k;
-	int* arr;
-	int* out;
-	bool suc;
+	int tasks_count, max_happ;
+	int* happ_values;
+	int* output;
+	int output_n;
 } Solution;
 
-int* check(const int* arr, int n, int k, int* f) {
-	int* o = (int*) calloc(n, sizeof(int));
-	for (int i = 0; i < n - 1; i++)
-		if (abs(arr[i] - arr[n - 1]) >= k) o[(*f)++] = i + 1;
-	return o;
+Solution s;
+
+void reset_solution()
+{
+	if (s.happ_values) free(s.happ_values);
+	if (s.output) free(s.output);
+	s.tasks_count = 0;
+	s.max_happ = 0;
+	s.happ_values = NULL;
+	s.output = NULL;
+	s.output_n = 0;
 }
 
-int eval_best(int n_o , const int* o, int i)
+void clear_stdin(int items)
 {
-	if (o == NULL) return 0;
-	int b = 100;
-	for (int j = 0; j < n_o; j++)
+	for(int i = 0; i < items; i++) scanf("%d", malloc(0));
+}
+
+int get_happ(int output, int current_task_index)
+{
+	// Horna cast pozice tasku a output
+	double higher_frac = (current_task_index - output) / 2.0;
+	int higher = (double)higher_frac != (int)higher_frac ? (int)higher_frac + 1 : (int)higher_frac;
+	// Dolna cast output
+	int lower = (int)(output / 2.0) + 1;
+	return lower + higher;
+}
+
+// Kontrola ci momentalna radost neprekrocila maximum (tym padom koncime).
+int* check_happ_level(int n)
+{
+	if (s.happ_values == NULL) return NULL;
+	int* out = (int*) calloc(n, sizeof(int));
+	for (int i = 0; i < n; i++)
 	{
-		int tmp = (int) (floor((double) o[j] / 2) + 1 + ceil((double) (i - o[j]) / 2));
-		if (tmp < b) b = tmp;
+		int current_happiness = s.happ_values[i] - s.happ_values[n - 1];
+		if (ABS(current_happiness) < s.max_happ) continue;
+		out[s.output_n++] = i + 1;
 	}
-	return b;
+	return out;
 }
 
-void find_solution(Solution* s)
+// Video - 09:24 - linearne prehladavanie, minimum je nahraditelne takze nas az tak netrapi (12:35)
+int find_max_happ_task(int i)
 {
-	s->suc = false;
-	for (int i = 2; i <= s->kids; i++)
+	if (s.output == NULL) return 0;
+	int max = INT_MAX;
+	for (int j = 0; j < s.output_n; j++)
 	{
-		int n_o = 0;
-		s->out = check(s->arr, i, s->k, &n_o);
-		if (n_o != 0)
+		int tmp = (int) get_happ(s.output[j], i);
+		if (tmp < max) max = tmp;
+	}
+	return max;
+}
+
+// Side Note - Preskakovat ulohy mozeme len pokial sme nejake predchadzajuce vyriesili
+bool find_solution()
+{
+	for (int i = MIN_TASKS; i <= s.tasks_count; i++)
+		if ((s.output = check_happ_level(i)) && s.output_n != 0)
 		{
-			printf("%d\n", eval_best(n_o, s->out, i));
-			s->suc = true;
-			break;
+			printf("%d\n", find_max_happ_task(i));
+			return true;
 		}
-	}
+	return false;
 }
 
-int main() {
-	Solution s;
-	while (scanf("%d %d", &s.kids, &s.k) > 0) {
-		s.arr = (int*) calloc(s.kids, sizeof(int));
-		for (int i = 0; i < s.kids; i++) scanf("%d", &(s.arr[i]));
-		if (s.kids < 3)
-		{
-			printf("%d\n", s.kids);
+bool init()
+{
+	if (s.tasks_count <= MIN_TASKS)
+	{
+		clear_stdin(s.tasks_count);
+		printf("%d\n", s.tasks_count);
+		return false;
+	}
+	s.happ_values = (int*) calloc(s.tasks_count, sizeof(int));
+	assert(s.happ_values != NULL);
+	for (int i = 0; i < s.tasks_count; i++)
+	{
+		scanf("%d", &(s.happ_values[i]));
+		if (!RANGE(s.happ_values[i], MAX_HAPP+1, MIN_HAPP)) return false;
+	}
+	return true;
+}
+
+int main()
+{
+	while (scanf("%d %d", &s.tasks_count, &s.max_happ) > 0)
+	{
+		if (!RANGE(s.tasks_count, MAX_TASKS, MIN_TASKS-1) || !RANGE(s.max_happ, MAX_HAPP, MIN_HAPP+1) || !init())
 			continue;
-		}
-		find_solution(&s);
-		if (!s.suc) printf("%d\n", s.kids);
+		// Ked nenajdeme rozdiel medzi najvacsiou a najnizsiou happiness tak printujeme len pocet uloh.
+		if (!find_solution()) printf("%d\n", s.tasks_count);
+		reset_solution();
 	}
 	return 0;
 }
