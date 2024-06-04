74f0e5e537a068aba261edb05e33e19db46af763
Martin Szabo <martin.szabo333@gmail.com>
2024-06-04 07:05:00 +0200

    Dud

--- 5-2.c
+++ 5-2.c
@@ -1,99 +1,97 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#include <memory.h>
+
+// TODO:: dud
 
 #define SIZE 50
 
-char res[SIZE];
-
-void reset_matrix() { for (int i = 0; i < SIZE; i++) res[i] = '.'; }
-
-long long to_num(const char* arr, int len)
+typedef struct
 {
-	long long res_num = 0;
-	for (int i = 0; i < len; i++)
-	{
-		res_num <<= 1;
-		if (arr[i] == '<') res_num++;
-	}
-	return res_num;
+	char val[SIZE];
+	size_t len;
+} String;
+
+String result;
+
+void reset() {
+	memset(result.val, '.', SIZE);
 }
 
-void inc(long long* index, long long* section, long long multiplier)
+size_t to_num(const char* arr, int len, size_t num, size_t i)
+{
+	return i >= len || len == 0 ? num : to_num(arr, len, arr[i] == '<' ? num*2+1 : num*2, i + 1);
+}
+
+void inc(size_t* index, size_t* section, size_t multiplier)
 {
 	*index += multiplier * *section;
 	*section /= 4;
 }
-bool l_good(long long num, int len)
+bool l_bad(size_t num, int len)
 {
-	if (len == 0) return false;
-	long long power = (long long) pow(2, len - 1);
-	long long index = num < power ? 0 : power;
-	long long section = power / 4;
+	if (len == 0) return true;
+	size_t power = (size_t) pow(2, len - 1);
+	size_t index = num < power ? 0 : power;
+	size_t section = power / 4;
 
 	while (section >= 2)
 	{
-		if (num < index + 3 * section && num >= index + 2 * section) return false;
+		if (num < index + 3 * section && num >= index + 2 * section) return true;
 		else if (num < index + 4 * section && num >= index + 3 * section) inc(&index, &section, 3);
 		else if (num < index + 2 * section && num >= index + section) inc(&index, &section, 1);
 		else if (num < index + section && num >= index) section /= 4;
 	}
-	return num % 2 == 0;
+	return num % 2;
 }
-bool r_good(long long num, int len)
+bool r_bad(size_t num, int len)
 {
-	if (len == 0) return false;
-	long long power = (long long) pow(2, len - 1);
-	if (num < power) return false;
+	if (len == 0) return true;
+	size_t power = (size_t) pow(2, len - 1);
+	if (num < power) return true;
 
-	long long index = power - 1;
-	long long section = power / 4;
+	size_t index = power - 1;
+	size_t section = power / 4;
 
 	while (section >= 2)
 	{
-		if (num <= index + 3 * section && num > index + 2 * section) return false;
+		if (num <= index + 3 * section && num > index + 2 * section) return true;
 		else if (num <= index + 4 * section && num > index + 3 * section) inc(&index, &section, 3);
 		else if (num <= index + 2 * section && num > index + section) inc(&index, &section, 1);
 		else if (num <= index + section && num > index) section /= 4;
 	}
-	return true;
+	return false;
 }
 
 void eval(const char* arr, int len)
 {
+	// Vzdy miznu dvojicky
 	for (int i = 0; i < len; i+=2)
 	{
-		long long first_part = to_num(arr, i);
-		long long second_part = to_num(arr + i + 1, len - i - 1);
+		size_t first_part = to_num(arr, i, 0, 0);
+		size_t second_part = to_num(arr + i + 1, len - i - 1, 0, 0);
 
-		if (l_good(first_part, i)) first_part = -1;
-		if (r_good(second_part, len - i - 1)) second_part = -1;
+		if (!l_bad(first_part, i)) first_part = -1;
+		if (!r_bad(second_part, len - i - 1)) second_part = -1;
 
-		if (first_part != -1 && second_part != -1) res[i] = 'o';
+		if (first_part != -1 && second_part != -1) result.val[i] = 'o';
 	}
 }
 
-void get_input(char* arr, int* len)
-{
-	while (scanf("%c", &arr[*len]) >= 1)
-	{
-		if (arr[*len] == '\n') break;
-		(*len)++;
-	}
-}
+
 int main()
 {
-	int len = 1;
-	char arr[SIZE] = { 0 };
-
-	while (scanf("%c", &arr[0]) >= 1)
+	String input = { { 0 }, 1 };
+	while (scanf("%s", input.val) >= 1)
 	{
-		get_input(arr, &len);
-		reset_matrix();
-		eval(arr, len);
-		printf("%*.*s\n", len, len, res);
-		len = 1;
-		reset_matrix();
+		input.len = strlen(input.val);
+		eval(input.val, input.len);
+		// Vypise iba prvych "len" znakov "val".
+		// Zdroj: https://stackoverflow.com/questions/2239519/is-there-a-way-to-specify-how-many-characters-of-a-string-to-print-out-using-pri
+		printf("%*.*s\n", input.len, input.len, result.val);
+		input.len = 1;
+		reset();
 	}
 	return 0;
 }
